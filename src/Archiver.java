937050287e12f107feb75e2cdc7f559d847e95da
ZanRafal <01159225@pw.edu.pl>
2021-08-04 23:28:22 +0200

    Added

---
+++ src/Archiver.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+
+public class Archiver {
+    public static void main(String[] args) throws Exception{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Please enter full file path:");
+        String path = reader.readLine();
+        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(path));
+
+        System.out.println("Enter full file name for archiving:");
+        String fileName = reader.readLine();
+        zipFileManager.createZip(Paths.get(fileName));
+    }
+}
