6e29d02a5d043c5253d02f1d8294e037e9e9342e
ZanRafal <01159225@pw.edu.pl>
2021-08-05 00:08:46 +0200

    Added ConsoleHelper and Operation classes

---
+++ src/ConsoleHelper.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleHelper {
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() throws IOException {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            return reader.readLine();
+    }
+
+    public static int readInt() throws IOException {
+        String text = readString();
+        return Integer.parseInt(text);
+    }
+}
