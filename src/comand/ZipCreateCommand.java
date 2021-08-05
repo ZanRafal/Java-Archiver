32a872f691a65a065bf0cb12ed87d93c1dbd98f3
ZanRafal <01159225@pw.edu.pl>
2021-08-05 23:48:35 +0200

    Added logic of ZipCreateCommand execute() method

--- src/comand/ZipCreateCommand.java
+++ src/comand/ZipCreateCommand.java
@@ -1,14 +1,25 @@
 package comand;
 
+import exception.PathNotFoundException;
 import main.ConsoleHelper;
 import main.ZipFileManager;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class ZipCreateCommand extends ZipCommand {
 
     @Override
     public void execute() throws Exception {
-        ConsoleHelper.writeMessage("Creating an archive.");
+        try {
+            ConsoleHelper.writeMessage("Creating an archive.");
+            ZipFileManager zipFileManager = getZipFileManager();
+            ConsoleHelper.writeMessage("Enter full path to the file or directory to be zipped.");
+            Path path = Paths.get(ConsoleHelper.readString());
+            zipFileManager.createZip(path);
+            ConsoleHelper.writeMessage("Archive created.");
+        } catch (PathNotFoundException exception) {
+            ConsoleHelper.writeMessage("You didn't correctly enter a file name or directory.");
+        }
     }
 }
