8e2be3d7af2a8fd646faf8211990cf8544fc424f
ZanRafal <01159225@pw.edu.pl>
2021-08-05 23:38:51 +0200

    Added logic of getZipFileManager method in ZipCommand

--- src/comand/ZipCommand.java
+++ src/comand/ZipCommand.java
@@ -1,5 +1,17 @@
 package comand;
 
+import main.ConsoleHelper;
+import main.ZipFileManager;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public abstract class ZipCommand implements Command {
 
+    public ZipFileManager getZipFileManager() throws Exception {
+        ConsoleHelper.writeMessage("Please enter the full path to the archive.");
+        String text = ConsoleHelper.readString();
+        Path filePath = Paths.get(text);
+        return new ZipFileManager(filePath);
+    }
 }
--- src/comand/ZipCreateCommand.java
+++ src/comand/ZipCreateCommand.java
@@ -1,9 +1,14 @@
 package comand;
 
+import main.ConsoleHelper;
+import main.ZipFileManager;
+
+import java.nio.file.Path;
+
 public class ZipCreateCommand extends ZipCommand {
 
     @Override
     public void execute() throws Exception {
-
+        ConsoleHelper.writeMessage("Creating an archive.");
     }
 }
