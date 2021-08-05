da6979f09aca4ea2afc36beaaa7d520dede64bf8
ZanRafal <01159225@pw.edu.pl>
2021-08-05 16:50:05 +0200

    Refactored main method, added askOperation method

--- src/main/Archiver.java
+++ src/main/Archiver.java
@@ -1,24 +1,36 @@
 package main;
 
-import comand.ExitCommand;
+import exception.NoSuchZipFileException;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.nio.file.Paths;
+import java.io.IOException;
 
 public class Archiver {
+
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("");
+        ConsoleHelper.writeMessage("Select an operation:");
+        ConsoleHelper.writeMessage(String.format("\t %d - Zip files to an archive", Operation.CREATE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - Add a file to an archive", Operation.ADD.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - Remove a file from an archive", Operation.REMOVE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - Extract an archive", Operation.EXTRACT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - View the contents of an archive", Operation.CONTENT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - Exit", Operation.EXIT.ordinal()));
+
+        return Operation.values()[ConsoleHelper.readInt()];
+    }
+
     public static void main(String[] args) throws Exception{
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Operation operation = null;
 
-        System.out.println("Please enter full file path:");
-        String path = reader.readLine();
-        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(path));
-
-        System.out.println("Enter full file name for archiving:");
-        String fileName = reader.readLine();
-        zipFileManager.createZip(Paths.get(fileName));
-
-        ExitCommand exitCommand = new ExitCommand();
-        exitCommand.execute();
+        do {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (NoSuchZipFileException e) {
+                ConsoleHelper.writeMessage("You didn't select an archive or you selected an invalid file.");
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("An error occurred. Please check the entered data.");
+            }
+        } while(operation != Operation.EXIT);
     }
 }
