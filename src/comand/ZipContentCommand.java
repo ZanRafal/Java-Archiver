1db3fbada724de0a0258a9f05874392e52d49486
ZanRafal <01159225@pw.edu.pl>
2021-08-06 20:30:29 +0200

    Added logic of ZipContentCommand's execute method logic

--- src/comand/ZipContentCommand.java
+++ src/comand/ZipContentCommand.java
@@ -1,9 +1,26 @@
 package comand;
 
+import main.ConsoleHelper;
+import main.FileProperties;
+import main.ZipFileManager;
+
+import java.util.List;
+
 public class ZipContentCommand extends ZipCommand {
 
+    //Displaying content of archive
     @Override
     public void execute() throws Exception {
+        ConsoleHelper.writeMessage("Viewing contents of the archive.");
+        ZipFileManager zipFileManager = getZipFileManager();
 
+        ConsoleHelper.writeMessage("Archive contents:");
+        List<FileProperties> files = zipFileManager.getFileList();
+
+        for (FileProperties item : files) {
+            ConsoleHelper.writeMessage(item.toString());
+        }
+
+        ConsoleHelper.writeMessage("Archive contents viewed.");
     }
 }
