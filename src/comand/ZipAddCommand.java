0f5a0ad5230e00a81ff4a9dd4e47149af48ec6bc
ZanRafal <01159225@pw.edu.pl>
2021-08-07 13:52:41 +0200

    Added logic of ZipAddCommand class's execute method

--- src/comand/ZipAddCommand.java
+++ src/comand/ZipAddCommand.java
@@ -1,9 +1,29 @@
 package comand;
 
+import exception.PathNotFoundException;
+import main.ConsoleHelper;
+import main.ZipFileManager;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ZipAddCommand extends ZipCommand {
 
     @Override
     public void execute() throws Exception {
 
+        try {
+            ConsoleHelper.writeMessage("Adding a new file to an archive.");
+            ZipFileManager zipFileManager = getZipFileManager();
+
+            ConsoleHelper.writeMessage("Enter full name of the file to be added.");
+            Path path = Paths.get(ConsoleHelper.readString());
+            zipFileManager.addFile(path);
+
+            ConsoleHelper.writeMessage("File added to an archive.");
+
+        } catch (PathNotFoundException exception) {
+            ConsoleHelper.writeMessage("File not found.");
+        }
     }
 }
